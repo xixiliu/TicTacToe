340ff68de187644d07ba4a413a729d3a1ef151b8
Kat Sullivan <kmsullivan012@gmail.com>
2015-11-04 14:06:19 -0500

    I'm deleting your code hahaha

--- main.js
+++ main.js
@@ -147,6 +147,7 @@ function checkForWinner(player) {
 }
 
 // restart the game 
+// lalala
 function restart() {
 	// clear the playing area
 	context.clearRect ( 0 , 0 , canvas.width, canvas.height );
